d801730105fd6877dfad711a58075cbda2364927
Yuri Kravcenko <1843084+1ur11@users.noreply.github.com>
2025-05-13 13:18:14 +0300

    fix tests

--- src/aggregator/aggregator.controller.spec.ts
+++ src/aggregator/aggregator.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AggregatorController } from './aggregator.controller';
 import { AggregatorService } from './aggregator.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { AgrTransaction } from './schemas/agr-transaction.schema';
+
+const mockAgrTransactionModel = {};
 
 describe('AggregatorController', () => {
   let controller: AggregatorController;
@@ -8,7 +12,13 @@ describe('AggregatorController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AggregatorController],
-      providers: [AggregatorService],
+      providers: [
+        AggregatorService,
+        {
+          provide: getModelToken(AgrTransaction.name),
+          useValue: mockAgrTransactionModel,
+        },
+      ],
     }).compile();
 
     controller = module.get<AggregatorController>(AggregatorController);
--- src/aggregator/aggregator.service.spec.ts
+++ src/aggregator/aggregator.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AggregatorService } from './aggregator.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { AgrTransaction } from './schemas/agr-transaction.schema';
+
+const mockAgrTransactionModel = {};
 
 describe('AggregatorService', () => {
   let service: AggregatorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AggregatorService],
+      providers: [
+        AggregatorService,
+        {
+          provide: getModelToken(AgrTransaction.name),
+          useValue: mockAgrTransactionModel,
+        },
+      ],
     }).compile();
 
     service = module.get<AggregatorService>(AggregatorService);
